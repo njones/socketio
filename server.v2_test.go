be7ea2909db62c9d4ea49489ef17c14227f6bc76
Nika Jones <github@nikajon.es>
2023-03-08 06:38:30 +0000

    Update tests to allow for auto-connect namespaces
    The tests before were suppressing polling auto-connect messages. Those are now being captured, and the tests have been updated to account for the extra messages.

--- server.v2_test.go
+++ server.v2_test.go
@@ -158,9 +158,9 @@ func SendingToTheClientV2(t *testing.T) []testDataOptFunc {
 
 		want = map[string][][]string{
 			"grab1": {
-				{`42["hello","can you hear me?",1,2,"abc"]`},
-				{`42["hello","can you hear me?",1,2,"abc"]`},
-				{`42["hello","can you hear me?",1,2,"abc"]`},
+				{`40`, `6`, `42["hello","can you hear me?",1,2,"abc"]`},
+				{`40`, `6`, `42["hello","can you hear me?",1,2,"abc"]`},
+				{`40`, `6`, `42["hello","can you hear me?",1,2,"abc"]`},
 			},
 		}
 		count = len(want["grab1"])
@@ -194,9 +194,9 @@ func SendingToAllClientsExceptTheSenderV2(t *testing.T) []testDataOptFunc {
 
 		want = map[string][][]string{
 			"grab1": {
-				{`42["broadcast","Hello friends!"]`},
-				{`42["broadcast","Hello friends!"]`},
-				nil,
+				{`40`, `6`, `42["broadcast","Hello friends!"]`},
+				{`40`, `6`, `42["broadcast","Hello friends!"]`},
+				{`40`, `6`},
 			},
 		}
 		count = len(want["grab1"])
@@ -232,11 +232,11 @@ func SendingToAllClientsInGameRoomExceptSenderV2(t *testing.T) []testDataOptFunc
 
 		want = map[string][][]string{
 			"grab1": {
-				{`42["nice game","let's play a game"]`},
-				nil,
-				{`42["nice game","let's play a game"]`},
-				nil,
-				nil, // sender...
+				{`40`, `6`, `42["nice game","let's play a game"]`},
+				{`40`, `6`},
+				{`40`, `6`, `42["nice game","let's play a game"]`},
+				{`40`, `6`},
+				{`40`, `6`}, // sender...
 			},
 		}
 		count = len(want["grab1"])
@@ -275,19 +275,19 @@ func SendingToAllClientsInGame1AndOrGame2RoomExceptSenderV2(t *testing.T) []test
 
 		want = map[string][][]string{
 			"grab1": {
-				{`42["nice game","let's play a game (too)"]`},
-				nil,
-				{`42["nice game","let's play a game (too)"]`},
-				{`42["nice game","let's play a game (too)"]`},
-				{`42["nice game","let's play a game (too)"]`},
-				nil,
-				{`42["nice game","let's play a game (too)"]`},
-				nil,
-				{`42["nice game","let's play a game (too)"]`},
-				{`42["nice game","let's play a game (too)"]`},
-				{`42["nice game","let's play a game (too)"]`},
-				nil,
-				nil, // sender...
+				{`40`, `6`, `42["nice game","let's play a game (too)"]`},
+				{`40`, `6`},
+				{`40`, `6`, `42["nice game","let's play a game (too)"]`},
+				{`40`, `6`, `42["nice game","let's play a game (too)"]`},
+				{`40`, `6`, `42["nice game","let's play a game (too)"]`},
+				{`40`, `6`},
+				{`40`, `6`, `42["nice game","let's play a game (too)"]`},
+				{`40`, `6`},
+				{`40`, `6`, `42["nice game","let's play a game (too)"]`},
+				{`40`, `6`, `42["nice game","let's play a game (too)"]`},
+				{`40`, `6`, `42["nice game","let's play a game (too)"]`},
+				{`40`, `6`},
+				{`40`, `6`}, // sender...
 			},
 		}
 		count = len(want["grab1"])
@@ -329,11 +329,11 @@ func SendingToAllClientsInGameRoomIncludingSenderV2(t *testing.T) []testDataOptF
 
 		want = map[string][][]string{
 			"grab1": {
-				{`42["big-announcement","the game will start soon"]`},
-				nil,
-				{`42["big-announcement","the game will start soon"]`},
-				nil,
-				{`42["big-announcement","the game will start soon"]`},
+				{`40`, `6`, `42["big-announcement","the game will start soon"]`},
+				{`40`, `6`},
+				{`40`, `6`, `42["big-announcement","the game will start soon"]`},
+				{`40`, `6`},
+				{`40`, `6`, `42["big-announcement","the game will start soon"]`},
 			},
 		}
 		count = len(want["grab1"])
@@ -379,11 +379,11 @@ func SendingToAllClientsInNamespaceMyNamespaceIncludingSenderV2(t *testing.T) []
 				{`40/myNamespace,`},
 			},
 			"grab1": {
-				{`42/myNamespace,["bigger-announcement","the tournament will start soon"]`},
-				nil,
-				{`42/myNamespace,["bigger-announcement","the tournament will start soon"]`},
-				nil,
-				{`42/myNamespace,["bigger-announcement","the tournament will start soon"]`},
+				{`40`, `6`, `42/myNamespace,["bigger-announcement","the tournament will start soon"]`},
+				{`40`, `6`},
+				{`40`, `6`, `42/myNamespace,["bigger-announcement","the tournament will start soon"]`},
+				{`40`, `6`},
+				{`40`, `6`, `42/myNamespace,["bigger-announcement","the tournament will start soon"]`},
 			},
 		}
 		count = len(want["send1"])
@@ -438,11 +438,11 @@ func SendingToASpecificRoomInNamespaceMyNamespaceIncludingSenderV2(t *testing.T)
 				{`40/myNamespace,`},
 			},
 			"grab1": {
-				{`42/myNamespace,["event","message"]`},
-				nil,
-				nil,
-				nil,
-				{`42/myNamespace,["event","message"]`},
+				{`40`, `6`, `42/myNamespace,["event","message"]`},
+				{`40`, `6`},
+				{`40`, `6`},
+				{`40`, `6`},
+				{`40`, `6`, `42/myNamespace,["event","message"]`},
 			},
 		}
 		count = len(want["send1"])
@@ -497,9 +497,9 @@ func SendingToIndividualSocketIDPrivateMessageV2(t *testing.T) []testDataOptFunc
 
 		want = map[string][][]string{
 			"grab1": {
-				{`42["hey","I just met you #1"]`},
-				{`42["hey","I just met you #2"]`},
-				{`42["hey","I just met you #0"]`},
+				{`40`, `6`, `42["hey","I just met you #1"]`},
+				{`40`, `6`, `42["hey","I just met you #2"]`},
+				{`40`, `6`, `42["hey","I just met you #0"]`},
 			},
 		}
 		count = len(want["grab1"])
@@ -542,7 +542,7 @@ func SendingWithAcknowledgementV2(t *testing.T) []testDataOptFunc {
 		wait = new(sync.WaitGroup)
 
 		want = map[string][][]string{
-			"grab1": {{`421["question","do you think so?"]`}},
+			"grab1": {{`40`, `6`, `421["question","do you think so?"]`}},
 			"send2": {{`431["answer",42]`}},
 		}
 		count = len(want["grab1"])
@@ -592,11 +592,11 @@ func SendingToAllConnectedClientsV2(t *testing.T) []testDataOptFunc {
 
 		want = map[string][][]string{
 			"grab1": {
-				{`42["*","an event sent to all connected clients"]`},
-				{`42["*","an event sent to all connected clients"]`},
-				{`42["*","an event sent to all connected clients"]`},
-				{`42["*","an event sent to all connected clients"]`},
-				{`42["*","an event sent to all connected clients"]`},
+				{`40`, `6`, `42["*","an event sent to all connected clients"]`},
+				{`40`, `6`, `42["*","an event sent to all connected clients"]`},
+				{`40`, `6`, `42["*","an event sent to all connected clients"]`},
+				{`40`, `6`, `42["*","an event sent to all connected clients"]`},
+				{`40`, `6`, `42["*","an event sent to all connected clients"]`},
 			},
 		}
 		count = len(want["grab1"])
@@ -642,11 +642,11 @@ func OnEventV2(t *testing.T) []testDataOptFunc {
 				{`41`}, nil, nil, nil, nil,
 			},
 			"grab2": {
-				{`42["say goodbye","disconnecting..."]`},
-				nil,
-				{`42["say goodbye","disconnecting..."]`},
-				nil,
-				{`42["say goodbye","disconnecting..."]`},
+				{`40`, `6`, `42["say goodbye","disconnecting..."]`},
+				{`40`, `6`},
+				{`40`, `6`, `42["say goodbye","disconnecting..."]`},
+				{`40`, `6`},
+				{`40`, `6`, `42["say goodbye","disconnecting..."]`},
 			},
 		}
 		count = len(want["send1"])
@@ -704,7 +704,7 @@ func RejectTheClientV2(t *testing.T) []testDataOptFunc {
 
 		want = map[string][][]string{
 			"connect_query": {{`access=true`}, {`access=false`}},
-			"grab1":         {{`42["hello",1]`}, {`44{"message":"not authorized"}`}},
+			"grab1":         {{`40`, `6`, `42["hello",1]`}, {`40`, `6`, `44{"message":"not authorized"}`}},
 		}
 		count = len(want["connect_query"])
 		cnt   = int64(0)
